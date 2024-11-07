340f47ab45526232e196afad9a65321faf28a94b
Richie000501 <richie9505051@gmail.com>
2024-11-07 22:16:38 +0800

    d

--- hw5_35/hw5_35/main.c
+++ hw5_35/hw5_35/main.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h>
 
-unsigned long int fibonacci(unsigned int n)
-{
-	unsigned long int a = 0, b = 1, total;
-	if (n == 0)
-	{
-		return 0;
-	}
-	if (n == 1)
-	{
-		return 1;
-	}
-
-	for (int i = 3; i <= n; i++)
-	{
-		total = a + b;
-		a = b;
-		b = total;
-	}
-	return total;
+unsigned long long int fibonacci(unsigned int n) {
+    unsigned long long int a = 0, b = 1, ans;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    while (--n) {
+        // notice that we cannot write "if(ULLONG_MAX < a + b)" due to overflow issue
+        if (ULLONG_MAX - a < b) {
+            return ULLONG_MAX;
+        }
+        ans = a + b;
+        a = b;
+        b = ans;
+    }
+    return ans;
 }
-int main()
-{
-	int n;
-	printf("稱笵fibonacci计材n:");
-	scanf_s("%d", &n);
-	unsigned long int result = fibonacci(n);
-	printf("材%d:%u\n", n,result);
-	unsigned long long c = 0, d = 1, fib = 1;
-	unsigned int count = 1;
-	while (fib <= ULLONG_MAX - d) { 
-		fib = c + d;
-		c = d;
-		d = fib;
-		count++;
-	}
-	printf("╰参纗程 Fibonacci 兜计%u癸莱 Fibonacci 计%llu\n", count , c);
 
+// int main(){
+//     unsigned int n;
+//     scanf("%u", &n);
+//     printf("%llu", fibonacci(n));
+//     return 0;
+// }
 
+int main() {
+    unsigned int n = 0, max_n;
+    unsigned long long int val, max_val;
+    // scanf("%u", &n);
+    while (1) {
+        val = fibonacci(n);
+        if (val == ULLONG_MAX) break;
+        printf("n = %u, val = %llu\n", n, val);
+        max_val = val;
+        max_n = n;
+        n++;
+    }
+    printf("maximum value of unsigned long long int = %llu\n", ULLONG_MAX);
+    printf("max_n = %u, max_val = %llu", max_n, max_val);
+    return 0;
 }
+
